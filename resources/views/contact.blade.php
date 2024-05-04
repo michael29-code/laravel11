1f28b77cf90adf3545efb2466e5298e9e8ce41e3
michael29-code <alf.michael29@gmail.com>
2024-05-04 16:42:53 +0700

    init

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+    <h1>Contact</h1>
+
+    <ul>
+        <li>Instagram : <a href="">Michael </a></li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about',['nama' => 'Michael']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
