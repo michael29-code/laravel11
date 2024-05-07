ec80bed9bb3a9e5b02d749fd746bea8a69cc30cc
michael29-code <alf.michael29@gmail.com>
2024-05-07 12:20:05 +0700

    blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Us</title>
-    <link rel="stylesheet" href="css/styles.css">
-
-</head>
-<body>
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <a href="/blog">blog</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman about</h1>
-
-    <h1>{{ $nama }}<h1>
-    {{-- <img src="" alt=""> --}}
-    <script src="js/scripts.js"></script>
-
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">about {{ $nama }}</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,67 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',['title' => 'Home']);
 });
 Route::get('/about', function () {
-    return view('about',['nama' => 'Michael']);
+    return view('about',['nama' => 'Michael', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Michael',
+            'Body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis doloremque deserunt excepturi nemo
+            soluta corporis ratione unde tempora laborum harum?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Michael',
+            'Body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, aliquam vel accusantium exercitationem
+            cupiditate iure? Optio fuga, doloribus necessitatibus voluptatibus tempora dicta, provident aspernatur culpa
+            ab ex consequatur magnam harum!'
+        ],
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Michael',
+            'Body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis doloremque deserunt excepturi nemo
+            soluta corporis ratione unde tempora laborum harum?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Michael',
+            'Body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, aliquam vel accusantium exercitationem
+            cupiditate iure? Optio fuga, doloribus necessitatibus voluptatibus tempora dicta, provident aspernatur culpa
+            ab ex consequatur magnam harum!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post',['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
+
+
